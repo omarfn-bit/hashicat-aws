c6f2c9d6df000fb38ca238559d47f44b9501ea79
omarfn-bit <omarfn@gmail.com>
2023-10-13 22:03:09 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -128,7 +128,9 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+    Departament = "devops"
   }
+
 }
 
 # We're using a little trick here so we can run the provisioner without
